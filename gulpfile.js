853bf5ea30cdb062a8aa6ae306901aec656b1541
wilson.muktar <wilson.muktar@grabtaxi.com>
2024-05-29 14:32:12 +0800

    remove gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var path = require('path');
 var sass = require('gulp-sass')(require('sass'));
-var autoprefixer = require('gulp-autoprefixer');
+//var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 
