000268063ae2c089331fb27113fadb14e3c56daf
wilson.muktar <wilson.muktar@grabtaxi.com>
2024-06-09 15:15:53 +0800

    update catch icon

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -104,7 +104,7 @@ if (localStorage.getItem('user_roles') == "system_admin" || localStorage.getItem
             <li class="nav-item">
                 <a class="nav-link" href="../pages/catch.html">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-trophy text-success text-sm opacity-10"></i>
+                        <i class="fa fa-solid fa-fish text-success text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1" data-i18n-key="breadcrumb_catch">Catch</span>
                 </a>
