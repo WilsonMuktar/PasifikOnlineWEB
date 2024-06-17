f5e80420111755256d0d1784b85adca1fd3ef2d7
wilson.muktar <wilson.muktar@grabtaxi.com>
2024-06-17 12:17:38 +0800

    update sidebar

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -167,7 +167,7 @@ if (localStorage.getItem('user_roles') == "system_admin" || localStorage.getItem
                     <span class="nav-link-text ms-1" data-i18n-key="breadcrumb_stock">Stocks</span>
                 </a>
             </li>
-            <li class="nav-item" hidden>
+            <li class="nav-item">
                 <a class="nav-link" href="../pages/transaction.html">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
